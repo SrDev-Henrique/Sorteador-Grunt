f39aed6711b7f4bdffb5b51aadf9cea0043a73fa
Henrique Albuquerque <halbuquerque2850@gmail.com>
2024-11-12 15:55:56 -0300

    First commit

---
+++ src/scripts/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById('form-sorteador').addEventListener('submit', function (e) {
+        e.preventDefault();
+        let numeroMaximo = document.getElementById('numero-maximo').value;
+        numeroMaximo = parseInt(numeroMaximo);
+
+        let numeroAleatorio = Math.random() * numeroMaximo;
+        numeroAleatorio = Math.floor(numeroAleatorio + 1)
+
+        document.getElementById('resultado').innerText = numeroAleatorio;
+        document.querySelector(".resultado").style.opacity = '1';
+    })
+});
