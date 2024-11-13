ec58ef6862f87fe77c1814e1962345933a416ca7
Henrique Albuquerque <halbuquerque2850@gmail.com>
2024-11-13 14:41:42 -0300

    Add vercel.json

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,13 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById('form-sorteador').addEventListener('submit', function (e) {
-        e.preventDefault();
-        let numeroMaximo = document.getElementById('numero-maximo').value;
-        numeroMaximo = parseInt(numeroMaximo);
+  document
+    .getElementById("form-sorteador")
+    .addEventListener("submit", function (e) {
+      e.preventDefault();
+      let numeroMaximo = document.getElementById("numero-maximo").value;
+      numeroMaximo = parseInt(numeroMaximo);
 
-        let numeroAleatorio = Math.random() * numeroMaximo;
-        numeroAleatorio = Math.floor(numeroAleatorio + 1)
+      let numeroAleatorio = Math.random() * numeroMaximo;
+      numeroAleatorio = Math.floor(numeroAleatorio + 1);
 
-        document.getElementById('resultado').innerText = numeroAleatorio;
-        document.querySelector(".resultado").style.opacity = '1';
-    })
+      document.getElementById("resultado").innerText = numeroAleatorio;
+      document.querySelector(".resultado").style.opacity = "1";
+    });
 });
